69158e51e4548b1d08fa3023ac7dc3c251987047
pradow <pradochris+pradow@protonmail.com>
2021-08-11 10:48:18 -0700

    add bookshelf file and write functionality to populate books added

---
+++ src/js/bookshelf.js
@@ -0,0 +1,39 @@
+const bookshelf = (function () {
+  if (document.querySelector(".js-results")) {
+    const bookshelfContainer = document.querySelector(".js-results");
+    const bookshelfLink = document.querySelector(".js-bookshelf-link");
+    const userData = userDataFunctions.userData;
+
+    function removeElement(elementClicked) {
+      bookshelfContainer.removeChild(elementClicked.parentNode);
+    }
+
+    function emitRemoveBook(e) {
+      const buttonClicked = e.currentTarget;
+      const parentEl = buttonClicked.parentNode;
+
+      removeElement(buttonClicked);
+
+      events.emit("removeBook", parentEl.dataset.bookid);
+    }
+
+    function populateData(data) {
+      let htmlToAppend;
+      data.forEach((item) => {
+        htmlToAppend += `<div class='single-book' data-bookid="${item.id}"><h3 class='book-title'>${item.title}</h3>
+        <button class="js-addRemovebook remove-button">Remove from bookshelf</button>
+        </div>`;
+      });
+
+      bookshelfContainer.innerHTML = htmlToAppend;
+
+      document.querySelectorAll(".js-addRemovebook").forEach((item) => {
+        item.addEventListener("click", emitRemoveBook);
+      });
+    }
+    bookshelfLink.addEventListener("click", (e) => {
+      e.preventDefault();
+      populateData(userData.booksData);
+    });
+  }
+})();
